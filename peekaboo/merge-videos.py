11345f0563645ae08f4974d7e85aca5418f7cf0a
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2024-06-23 14:54:45 +0800

    Update merge-video script

--- peekaboo/merge-videos.py
+++ peekaboo/merge-videos.py
@@ -1,9 +1,9 @@
 #### Affiliation: SW-Lab, Dept of CFS, NTNU
 #### Project: Peekaboo (aka Omi2)
-#### Date: 21.06.2024 (long count: 13.0.11.12.0, 5 Ajpu')
+#### Date: 23.06.2024 (long count: 13.0.11.12.2, 7 Iq')
 #### Author: MY Sia (with lots of help from the web, see README.md for more)
 
-#### Aim of script: Concatenate short videos into a long one; batch process several cameras (BABY, SCREEN, & 3 SBRs) of several children
+#### Aim of script: Concatenate short videos into a long one; batch process several cameras of several children
 ##Input: Multiple videos (each has a duration of 1 minute) stored in a folder (one folder for each camera)
 ##Output: A single video per camera per child
 ##Recommended directory: project folder -> child folder -> camera folder -> short videos (e.g., <desktop>/Peekaboo/P01/BABY/<videos>)
@@ -12,7 +12,6 @@
 ########################################
 #### START HERE:
 ## Import everything needed to edit video clips
-## Remember to move all BABY capping videos to a sub-folder (ask the PI if you don't understand this part)
 import glob
 from pathlib import Path
 from moviepy.editor import *
@@ -22,84 +21,33 @@ print("Remember to MOVE CAPPING videos in the BABY folder to a sub-folder.")
 folder = "C:/Users/user/Desktop/peekaboo/peekadata" ##set path to the project folder
 
 ########################################
-#### LOOP THROUGH SEVERAL BABIES:
-group = ["064", "065", "066", "067", "068", "069"] ##which child folder are we processing?
+children = ["007", "P6"] ##which child folder are we processing?
+camera = ["BABY", "SCREEN", "SBR1", "SBR2", "SBR3"] ##which camera folder are we processing?
 problem = [] ##empty holder to store problematic IDs
 
-for child in group:
-    #### CONCATENATE BABY VIDEOS
-    baby_video_list = [] ##empty holder to store videos
-    baby_files_path = Path(f"{folder}/{child}/BABY") ##name of camera folder is BABY
-    baby_file_list = glob.glob(f"{baby_files_path}/*.mp4")
-    ## (a)Check for weird cases:
-    if len(baby_file_list) < 2:
-        x = f"No/only one video found in {child}'s BABY folder."
-        print(x) ##if the BABY folder is empty (i.e., no short videos), let us know
-        problem.append(x)
-    else:
-        ## (b)Load mp4 files as videos:
-        for i in baby_file_list:
-            print(i) ##which video is being processed?
-            clip = VideoFileClip(i)
-            baby_video_list.append(clip)
-        ## (c)Merge & save the output:
-        baby = concatenate_videoclips(baby_video_list)
-        new_baby = baby.set_fps(fps=30)
-        first_baby_frame = baby_file_list[0][-21:-15]
-        new_baby.write_videofile(f"{folder}/{child}/{child}_baby_{first_baby_frame}.mp4")
-        winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-        winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-        print(f"{child} BABY is done.")
-    #### CONCATENATE SCREEN VIDEOS
-    screen_video_list = [] ##empty holder to store videos
-    screen_files_path = Path(f"{folder}/{child}/SCREEN") ##name of camera folder is SCREEN
-    screen_file_list = glob.glob(f"{screen_files_path}/*.mp4")
-    ## (a)Check for weird cases:
-    if len(baby_file_list) < 2:
-        x = f"No/only one video found in {child}'s SCREEN folder."
-        print(x) ##if the SCREEN folder is empty, let us know
-        problem.append(x)
-    else:
-        ## (b)Load mp4 files as videos:
-        for i in screen_file_list:
-            print(i) ##which video is being processed?
-            clip = VideoFileClip(i)
-            screen_video_list.append(clip)
-        ## (c)Merge & save the output:
-        screen = concatenate_videoclips(screen_video_list)
-        new_screen = screen.set_fps(fps=30)#.fx(vfx.mirror_y)
-        first_screen_frame = screen_file_list[0][-21:-15]
-        new_screen.write_videofile(f"{folder}/{child}/{child}_screen_{first_screen_frame}.mp4")
-        winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-        winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-        print(f"{child} SCREEN is done.")
-    #### CONCATENATE SBR VIDEOS
-    n = 1 ##SBR camera number (we have 3)
-    while n < 4:
-        sbr_video_list = [] ##empty holder to store videos
-        sbr_files_path = Path(f"{folder}/{child}/SBR{n}") ##name of camera folder is SBR1,2,3
-        sbr_file_list = glob.glob(f"{sbr_files_path}/*.mp4")
-        ## (a)Check for weird cases:
-        if len(sbr_file_list) < 2:
-            x = f"No/only one video found in {child}'s SBR{n}."
-            print(x)
+#### Loop through baby folders
+for child in children:
+    ## Loop through camera folders
+    for cam in camera:
+        video_list = [] ##empty holder to store videos
+        files_path = Path(f"{folder}/{child}/{cam}")
+        file_list = glob.glob(f"{files_path}/*.mp4")
+        ## (a)Check for problematic cases:
+        if len(file_list) < 2:
+            x = f"Nothing to join in {child}'s {cam} folder."
             problem.append(x)
-            n += 1 ##if the SBR camera folder is empty (i.e., no short videos), move to the next one
         else:
             ## (b)Load mp4 files as videos:
-            for i in sbr_file_list:
+            for i in file_list:
                 print(i) ##which video is being processed?
                 clip = VideoFileClip(i)
-                sbr_video_list.append(clip)
+                video_list.append(clip)
             ## (c)Merge & save the output:
-            sbr = concatenate_videoclips(sbr_video_list)
-            new_sbr = sbr.set_fps(fps=30)
-            first_sbr_frame = sbr_file_list[0][-21:-15]
-            new_sbr.write_videofile(f"{folder}/{child}/{child}_sbr{n}_{first_sbr_frame}.mp4")
+            joined = concatenate_videoclips(video_list)
+            joined2 = joined.set_fps(fps=30) ##standardise frame per second for all videos
+            first_frame = file_list[0][-21:-15]
+            joined2.write_videofile(f"{folder}/{child}/{child}_{cam}_{first_frame}.mp4")
             winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-            winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-            print(f"{child} SBR{n} is done.")
-            n += 1 ##now, do the next SBR camera
 
 #### Were there any problematic camera folders?
 if len(problem) > 0:
