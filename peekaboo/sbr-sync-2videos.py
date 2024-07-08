d7bf38b0788e17b790ca32c8c8b3a96d70ea6b20
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2024-07-08 18:12:11 +0800

    Add files via upload

--- peekaboo/sbr-sync-2videos.py
+++ peekaboo/sbr-sync-2videos.py
@@ -1,6 +1,6 @@
 #### Affiliation: SW-Lab, Dept of CFS, NTNU
 #### Project: Peekaboo (aka Omi2)
-#### Date: 23.06.2024 (long count: 13.0.11.12.2, 7 Iq')
+#### Date: 08.07.2024 (long count: 13.0.11.12.17, 9 No'j)
 #### Author: MY Sia (with lots of help from the web, see README.md for more)
 
 #### Aim of script: Sync and combine two SBR (shared book reading) videos for PEER coding
@@ -22,23 +22,23 @@ import winsound ##not necessary, just to get Python to notify me when it is done
 folder = "C:/Users/user/Desktop/peekaboo/peekadata" ##set path to the project folder
 
 ########################################
+#### Set default values
 dur = 334 ##duration of the SBR is fixed: 5min 34sec
 problem = [] ##empty holder to store problematic IDs
 
 #### Enter information
-children = ["035", "038", "051", "066"] ##which child folder are we looping?
-major = [1, 2, 3, 2] ##the camera (1, 2, or 3) that has the best view of the parent-child dyad
-start = [20, 60, 25, 5] ##the seconds at which SBR STARTED in the MAIN video
-
+attempts = 2 ##the number of attempts in merging videos
+children = ["074", "075"] ##which child folder are we looping?
+major = [2, 1] ##the camera (1, 2, or 3) that has the best view of the parent-child dyad
+start = [29, 32] ##the seconds at which SBR STARTED in the MAIN video
 ## Manually correct out-of-sync minor videos:
-corr = [2, 1, -1.7, 1.3]
-##this is used only in the corrective rounds (i.e., second attempt or later)
-##if the MINOR video LAGS BEHIND, give a POSITIVE number; OTHERWISE, give a NEGATIVE number
-##a larger absolute number given will introduce a larger time difference between the videos
-attempts = int(input(f"Please enter the attempt number in merging videos. "))
+corr = [1, -0.7]
+## - this is used only in the corrective rounds (i.e., second attempt or later)
+## - if the MINOR video LAGS BEHIND, give a POSITIVE number; OTHERWISE, give a NEGATIVE number
+## - a larger absolute number given will introduce a larger time difference between the videos
 
-n = 0
 #### Check entry of information
+n = 0 ##to loop the information entered above
 if len(major) != len(children):
     print("The number of 'MAJOR' does not match the number of 'CHILDREN'.")
 elif len(start) != len(children):
@@ -53,56 +53,64 @@ else:
         vid_list = glob.glob(f"{vid_path}/{child}_sbr*.mp4")
         ## Check for problematic cases
         if len(vid_list) != 2:
-            x = f"There should only be 3 SBR videos in {child}'s folder."
+            x = f"There should only be 2 SBR videos in {child}'s folder."
             problem.append(x)
             n += 1 ##skip this problematic case
         else:
             ## Identify main and minor videos
             if vid_list[0][-15:-11] == f"sbr{major[n]}":
-                maj_n = 0
-                min_n = 1
+                maj_n, min_n = 0, 1
             else:
-                maj_n = 1
-                min_n = 0
-        ## The main video
-        major_vid = VideoFileClip(vid_list[maj_n])
-        t_major = major_list[maj_n][-10:-5]
-        t_major = t_major.replace("M", ":")
-        t_major = datetime.strptime(t_major, "%M:%S")
-        ## The minor video
-        minor_vid = VideoFileClip(vid_list[min_n], audio=False)
-        t_minor = minor_list[min_n][-10:-5]
-        t_minor = t_minor.replace("M", ":")
-        t_minor = datetime.strptime(t_minor, "%M:%S")
-        ## Get time difference between videos
-        diff = t_major - t_minor
-        diff = diff.total_seconds()
-        ## (b)Sync the videos:
-        end = start[n] + dur ##the seconds at which SBR should have ended in the main video
-        ## But some videos may not be long enough to be clipped off
-        if end > major_vid.duration:
-            major_vid = major_vid.subclip(start[n], major_vid.duration)
-        else:
-            major_vid = major_vid.subclip(start[n], end)
-        major_vid = major_vid.resize(0.7).volumex(3).margin(5) ##increase volume and add a border
-        if attempts == 1: ##first round
-            x = 0 ##no manual correction
-            if end+diff > minor_vid.duration:
-                minor_vid = minor_vid.subclip(start[n]+diff, minor_vid.duration)
+                maj_n, min_n = 1, 0
+            ## The main video
+            major_vid = VideoFileClip(vid_list[maj_n])
+            t_major = vid_list[maj_n][-10:-5]
+            t_major = t_major.replace("M", ":")
+            t_major = datetime.strptime(t_major, "%M:%S")
+            ## The minor video
+            minor_vid = VideoFileClip(vid_list[min_n])
+            t_minor = vid_list[min_n][-10:-5]
+            t_minor = t_minor.replace("M", ":")
+            t_minor = datetime.strptime(t_minor, "%M:%S")
+            ## Get time difference between videos
+            diff = t_major - t_minor
+            diff = diff.total_seconds()
+            ## (b)Sync the videos:
+            end = start[n] + dur ##the seconds at which SBR should have ended in the main video
+            ## But some videos may not be long enough to be clipped off
+            if end > major_vid.duration:
+                major_vid = major_vid.subclip(start[n], major_vid.duration)
             else:
-                minor_vid = minor_vid.subclip(start[n]+diff, end+diff)
-        elif attempts > 1: ##corrective round
-            x = corr[n]
-            if end+diff+corr[n] > minor_vid.duration:
-                minor_vid = minor_vid.subclip(start[n]+diff+corr[n], minor_vid.duration)
+                major_vid = major_vid.subclip(start[n], end)
+            ## Resize, set volumes, & border colour
+            if vid_list[maj_n][-15:-11] == f"sbr2":
+                vol_maj, vol_min = 6, 0
+                col_maj, col_min = (0, 0, 255), (0, 0, 0)
+            elif vid_list[min_n][-15:-11] == f"sbr2":
+                vol_maj, vol_min = 0, 6
+                col_maj, col_min = (0, 0, 0), (0, 0, 255)
             else:
-                minor_vid = minor_vid.subclip(start[n]+diff+corr[n], end+diff+corr[n])
-        minor_vid = minor_vid.resize(0.4).margin(8)
-        final_sbr = clips_array([[major_vid, minor_vid], ])
-        ## (c)Save the output:
-        final_sbr.write_videofile(f"{folder}/{child}/{child}_allSBR{attempts}_{start[n]}_corr={x}.mp4")
-        winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-        n += 1 ##now, do the next one
+                vol_maj, vol_min = 10, 0
+                col_maj, col_min = (0, 0, 255), (0, 0, 0)
+            major_vid = major_vid.resize(0.7).volumex(vol_maj).margin(8, color=col_maj)
+            if attempts == 1: ##first round
+                x = 0 ##no manual correction
+                if end+diff > minor_vid.duration:
+                    minor_vid = minor_vid.subclip(start[n]+diff, minor_vid.duration)
+                else:
+                    minor_vid = minor_vid.subclip(start[n]+diff, end+diff)
+            elif attempts > 1: ##corrective round
+                x = corr[n]
+                if end+diff+corr[n] > minor_vid.duration:
+                    minor_vid = minor_vid.subclip(start[n]+diff+corr[n], minor_vid.duration)
+                else:
+                    minor_vid = minor_vid.subclip(start[n]+diff+corr[n], end+diff+corr[n])
+            minor_vid = minor_vid.resize(0.4).volumex(vol_min).margin(8, color=col_min)
+            final_sbr = clips_array([[major_vid, minor_vid], ])
+            ## (c)Save the output:
+            final_sbr.write_videofile(f"{folder}/{child}/{child}_allSBR{attempts}_{start[n]}_corr={x}.mp4")
+            winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
+            n += 1 ##now, do the next one
 
 #### Were there any problematic camera folders?
 if len(problem) > 0:
--- peekaboo/sbr-sync-3videos.py
+++ peekaboo/sbr-sync-3videos.py
@@ -1,6 +1,6 @@
 #### Affiliation: SW-Lab, Dept of CFS, NTNU
 #### Project: Peekaboo (aka Omi2)
-#### Date: 05.07.2024 (long count: 13.0.11.12.14, 6 Ix')
+#### Date: 08.07.2024 (long count: 13.0.11.12.17, 9 No'j)
 #### Author: MY Sia (with lots of help from the web, see README.md for more)
 
 #### Aim of script: Sync and combine three SBR (shared book reading) videos for PEER coding
@@ -25,23 +25,15 @@ folder = "C:/Users/user/Desktop/peekaboo/peekadata" ##set path to the project fo
 #### Set default values
 dur = 334 ##duration of the SBR: 5min 34sec
 problem = [] ##empty holder to store problematic IDs
-## Set audio volume as 0 and border colour (RGB) as black
-## - will be updated in the codes later
-vol_mj = 0
-vol_mn1 = 0
-vol_mn2 = 0
-col_mj = (0, 0, 0)
-col_mn1 = (0, 0, 0)
-col_mn2 = (0, 0, 0)
 
 #### Enter information
-attempts = 4 ##the number of attempts in merging videos
-children = ["013", "074"] ##which child folder are we looping?
-main = [3, 2] ##the camera number (1, 2, or 3) that has the best view of the parent-child dyad
-start = [35, 29] ##the seconds at which SBR STARTED in the MAIN video
+attempts = 2 ##the number of attempts in merging videos
+children = ["079", "080", "081", "082", "083", "084", "085", "086"] ##which child folder are we looping?
+main = [3, 3, 1, 3, 3, 3, 3, 2] ##the camera number (1, 2, or 3) that has the best view of the parent-child dyad
+start = [34.5, 22, 55, 51.5, 19, 60, 35, 31] ##the seconds at which SBR STARTED in the MAIN video
 ## Manually correct out-of-sync minor videos:
-corr1 = [1.4, 1] ##to correct the minor-1 video (top right corner)
-corr2 = [1, 1.2] ##to correct the minor-2 video (bottom right corner)
+corr1 = [1, 0.8, -0.8, 2, 1, 1, 2, 0] ##to correct the minor-1 video (top right corner)
+corr2 = [-1, -0.8, 0.8, 1, 2, 1, 1, -1] ##to correct the minor-2 video (bottom right corner)
 ## - these are used only in the corrective rounds (i.e., second attempt or later)
 ## - if the MINOR video LAGS BEHIND, give a POSITIVE number; OTHERWISE, give a NEGATIVE number
 ## - a larger absolute number given will introduce a larger time difference between the videos
@@ -66,19 +58,20 @@ else:
             n += 1 ##skip this problematic case
         else:
             ## (a)Prepare videos:
+            ## Initial video settings: muted + black borders
+            vol_mj = vol_mn1 = vol_mn2 = 0
+            col_mj = col_mn1 = col_mn2 = (0, 0, 0)
             ## Identify the main video and the other two will be the minor videos
             allowed = [0, 1, 2]
             major = main[n] - 1
             allowed.remove(major)
-            minor1 = allowed[0]
-            minor2 = allowed[1]
+            minor1, minor2 = allowed[0], allowed[1]
             ## Search for SBR2-camera, which happened to have the clearest audio
-            ## - retain only SBR2's audio, the rest will be muted
-            ## - add a blue border to SBR2-camera
+            ## - retain only SBR2's audio, & add a blue border to SBR2-camera
             ## The main video
             major_vid = VideoFileClip(vid_list[major])
             if vid_list[major][-15:-11] == "sbr2":
-                vol_mj = 6 ##increase the volume by 6
+                vol_mj = 6
                 col_mj = (0, 0, 255)
             t_major = vid_list[major][-10:-5]
             t_major = t_major.replace("M", ":")
@@ -131,7 +124,7 @@ else:
                     minor_vid2 = minor_vid2.subclip(start[n]+diff2+corr2[n], minor_vid2.duration)
                 else:
                     minor_vid2 = minor_vid2.subclip(start[n]+diff2+corr2[n], end+diff2+corr2[n])
-            ## (c)Resize videos, add borders:
+            ## (c)Resize videos, change volume, add borders:
             final_min = clips_array([[minor_vid1.resize(width=480).volumex(vol_mn1).margin(8, color=col_mn1)],
                                      [minor_vid2.resize(width=480).volumex(vol_mn2).margin(8, color=col_mn2)]])
             final_all = clips_array([[major_vid.resize(height=720).volumex(vol_mj).margin(8, color=col_mj),
