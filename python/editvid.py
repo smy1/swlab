3bd526f31c003b3527514fa0c957f7ed9f934700
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2025-04-02 18:33:53 +0800

    Add files via upload

--- python/editvid.py
+++ python/editvid.py
@@ -1,7 +1,7 @@
 #### Affiliation: SW-Lab, Dept of CFS, NTNU
 #### Date: 31.03.2025
 #### Author: MY Sia (with lots of help from the web, see README.md for more)
-#### Functions: (1) concatenate, (2) sync and overlay
+#### Functions: (1) concatenate, (2) sync and overlay, (3) crop, (4) juxtapose
 
 #### Import necessary modules
 import glob
@@ -17,7 +17,7 @@ from openpyxl import load_workbook ##if we extract info from an excel file
 ##Date: first written on 23.06.2024, further editing on 12.02.2025
 ##Input: Multiple videos stored in a folder (one folder for each camera)
 ##Output: A single video per camera per child
-##Required directory: project folder -> child folder -> camera folder -> short videos (e.g., <desktop>/Peekaboo/P01/BABY/<videos>)
+##Required directory: project folder -> "child" subfolder -> "camera" subfolder -> short videos
 def merge(folder, children, camera):
     #### Loop through child folders
     for child in children:
@@ -64,7 +64,7 @@ def merge(folder, children, camera):
 ##Date: first written on 23.06.2024
 ##Input: A video showing the child's face (BABY video) and a video showing the screen that the child is watching (SCREEN video)
 ##Output: A single video per child with the BABY video as the main video and the SCREEN video overlaid on the top left corner
-##Required directory: project folder -> child folder -> videos (e.g., <desktop>/Peekaboo/P01/<videos>)
+##Required directory: project folder -> "child" subfolder -> videos (e.g., <desktop>/Peekaboo/P01/<videos>)
 def overlay(folder, attempts, bgcam, topcam, newname, propsize, dur,
             excel, children, start, end, corr):
     ### Extract information if we load an excel file
@@ -103,44 +103,50 @@ def overlay(folder, attempts, bgcam, topcam, newname, propsize, dur,
         ## (a)Prepare videos:
         vid_path = Path(f"{folder}/{child}/")
         ## Baby video
-        baby_list = glob.glob(f"{vid_path}/{child}_{bgcam}*.mp4")
-        baby_vid = VideoFileClip(baby_list[0])
-        t_baby = baby_list[0][-10:-5]
-        t_baby = t_baby.replace("M", ":")
-        t_baby = datetime.strptime(t_baby, "%M:%S")
-        ## Screen video
-        screen_list = glob.glob(f"{vid_path}/{child}_{topcam}*.mp4")
-        screen_vid = VideoFileClip(screen_list[0])
-        screen_vid = screen_vid.resize(propsize).margin(5) ##downsize video and add a 5px border
-        t_screen = screen_list[0][-10:-5]
-        t_screen = t_screen.replace("M", ":")
-        t_screen = datetime.strptime(t_screen, "%M:%S")
-        ## Get time difference between videos
-        diff = t_screen - t_baby
-        diff = diff.total_seconds()
-        ## (b)Overlay screen video on baby video at the top left corner:
-        if attempts == 1: ##first round
-            x = 0 ##no manual correction
-            all_vid = CompositeVideoClip([baby_vid.subclip(start[n]+diff, end[n]+diff),
-                                        screen_vid.subclip(start[n], end[n]).set_position((0, 50))])
-        elif attempts > 1: ##corrective round
-            x = corr[n]
-            all_vid = CompositeVideoClip([baby_vid.subclip(start[n]+diff+corr[n], end[n]+diff+corr[n]),
-                                        screen_vid.subclip(start[n], end[n]).set_position((0, 50))])
-        ## (c)Save composite video:
-        all_vid.write_videofile(f"{folder}/{child}/{child}_{newname}_merged{attempts}_corr={x}.mp4")
-        n += 1 ##now, do the next one
+        baby_list = glob.glob(f"{vid_path}/*{bgcam}*.mp4")
+        if len(baby_list) < 1: ##check for problem
+            print(f"Can't find {child}'s {bgcam}. Please check the path and folder names.")
+        else:
+            baby_vid = VideoFileClip(baby_list[0])
+            t_baby = baby_list[0][-10:-5]
+            t_baby = t_baby.replace("M", ":")
+            t_baby = datetime.strptime(t_baby, "%M:%S")
+            ## Screen video
+            screen_list = glob.glob(f"{vid_path}/*{topcam}*.mp4")
+            if len(screen_list) < 1: ##check for problem
+                print(f"Can't find {child}'s {topcam}. Please check the path and folder names.")
+            else:
+                screen_vid = VideoFileClip(screen_list[0])
+                screen_vid = screen_vid.resize(propsize).margin(5) ##downsize video and add a 5px border
+                t_screen = screen_list[0][-10:-5]
+                t_screen = t_screen.replace("M", ":")
+                t_screen = datetime.strptime(t_screen, "%M:%S")
+                ## Get time difference between videos
+                diff = t_screen - t_baby
+                diff = diff.total_seconds()
+                ## (b)Overlay screen video on baby video at the top left corner:
+                if attempts == 1: ##first round
+                    x = 0 ##no manual correction
+                    all_vid = CompositeVideoClip([baby_vid.subclip(start[n]+diff, end[n]+diff),
+                                                screen_vid.subclip(start[n], end[n]).set_position((0, 50))])
+                elif attempts > 1: ##corrective round
+                    x = corr[n]
+                    all_vid = CompositeVideoClip([baby_vid.subclip(start[n]+diff+corr[n], end[n]+diff+corr[n]),
+                                                screen_vid.subclip(start[n], end[n]).set_position((0, 50))])
+                ## (c)Save composite video:
+                all_vid.write_videofile(f"{folder}/{child}/{child}_{newname}_merged{attempts}_corr={x}.mp4")
+                n += 1 ##now, do the next one
 
 
 #### END OF FUNCTION 2 ####
 
 #### FUNCTION 3: CROP a video
-##Date: first written on
+##Date: first written on 18.02.2025
 ##Input:
 ##Output:
-##Required directory: project folder -> child folder -> videos (e.g., <desktop>/Peekaboo/P01/<videos>)
+##Required directory: project folder -> "child" subfolder -> videos
 def crop(folder, cam, newname, dur, amplify,
-         excel, children, start, end, x1, y1, x2, y2):
+         excel, children, start, end, x1, x2, y1, y2):
     if excel != None:
         wb = load_workbook(excel)
         sheet = wb["Sheet1"]
@@ -150,12 +156,12 @@ def crop(folder, cam, newname, dur, amplify,
         for i in list_children[1:]:
             children.append(i.value)
         #
-        start=[] ##the seconds at which the experiment STARTED in the SCREEN video
+        start=[] ##the seconds at which the experiment STARTED
         list_start =  sheet["b"]
         for i in list_start[1:]:
             start.append(i.value)
         #
-        end=[] ##the seconds at which the experiment ENDED in the SCREEN video
+        end=[] ##the seconds at which the experiment ENDED
         list_end =  sheet["c"]
         for i in list_end[1:]:
             end.append(i.value)
@@ -186,28 +192,167 @@ def crop(folder, cam, newname, dur, amplify,
         for i in start:
             end.append(i + dur)
         #
-    ### Start overlaying videos
+    ##Start overlaying videos
     n = 0
     for child in children:
         ## Prepare video:
         vid_path = Path(f"{folder}/{child}/")
-        vid_list = glob.glob(f"{vid_path}/{cam}*.mp4")
-        thevid = VideoFileClip(vid_list[0])
-        if thevid.duration > end[n]:
-            thevid = thevid.subclip(start[n], end[n])
+        vid_list = glob.glob(f"{vid_path}/*{cam}*.mp4")
+        if len(vid_list) < 1: ##check for problem
+            print(f"Can't find {child}'s {cam}. Please check the path and folder names.")
         else:
-            thevid = thevid.subclip(start[n], thevid.duration)
-        ## Check the size of the video and crop it:
-        (x, y) = thevid.size
-        file = open(f"{child}_{cam}.txt", "w")
-        file.write(f"{child}'s {cam} video has a size of {x} x {y}.")
-        file.close()
-        print(f"{child}'s {cam} has a size of {x} x {y}.")
-        cropped = thevid.crop(x1=x1[n], y1=y1[n], x2=x2[n], y2=y2[n])
-        cropped = cropped.volumex(amplify)
-        ## Extract and render solo video:
-        cropped.write_videofile(f"{folder}/{child}/{child}_{newname}.mp4")
-        n += 1 ##now, do the next one
+            thevid = VideoFileClip(vid_list[0])
+            if thevid.duration > end[n]:
+                thevid = thevid.subclip(start[n], end[n])
+            else:
+                thevid = thevid.subclip(start[n], thevid.duration)
+            ## Crop it:
+            cropped = thevid.crop(x1=x1[n], x2=x2[n], y1=y1[n], y2=y2[n])
+            cropped = cropped.volumex(amplify)
+            ## Extract and render:
+            cropped.write_videofile(f"{folder}/{child}/{child}_{newname}.mp4")
+            n += 1 ##now, do the next one
 
 
 #### END OF FUNCTION 3 ####
+
+#### FUNCTION 4: JUXTAPOSE videos
+##Date: first written on 08.07.2024, further editing on 18.02.2025
+##Input: Two video recordings (from different angles) that record the participant performing a task
+##Output: A single video per participant with one of the recordings (the one with the best angle) as the main video and the other as the minor
+##Required directory: project folder -> "child" subfolder -> videos
+def join2side(folder, attempts, cam1, cam2, newname, dur, amplify_who, amplify, mute_who, crop_who,
+              excel, children, main, start, end, corr, x1, x2, y1, y2):
+    if excel != None:
+        wb = load_workbook(excel)
+        sheet = wb["Sheet1"]
+        ##Extract information from excel
+        children=[] ##which child folder are we processing?
+        list_children = sheet["a"]
+        for i in list_children[1:]:
+            children.append(i.value)
+        #
+        main=[] ##the video with the best angle
+        list_main = sheet["b"]
+        for i in list_main[1:]:
+            main.append(i.value)
+        #
+        start=[] ##the seconds at which the experiment STARTED
+        list_start = sheet["c"]
+        for i in list_start[1:]:
+            start.append(i.value)
+        #
+        end=[] ##the seconds at which the experiment ENDED
+        list_end = sheet["d"]
+        for i in list_end[1:]:
+            end.append(i.value)
+        #
+        corr=[] ##the seconds at which the experiment ENDED
+        list_corr = sheet["e"]
+        for i in list_corr[1:]:
+            corr.append(i.value)
+        #
+        x1=[]
+        list_x1 = sheet["f"]
+        for i in list_x1[1:]:
+            x1.append(i.value)
+        #
+        x2=[]
+        list_x2 = sheet["g"]
+        for i in list_x2[1:]:
+            x2.append(i.value)
+        #
+        y1=[]
+        list_y1 = sheet["h"]
+        for i in list_y1[1:]:
+            y1.append(i.value)
+        #
+        y2=[]
+        list_y2 = sheet["i"]
+        for i in list_y2[1:]:
+            y2.append(i.value)
+        #
+    ##If all videos are of the same length, use it to calculate the end time
+    if dur != None:
+        end = []
+        for i in start:
+            end.append(i + dur)
+        #
+    ##Start juxtaposing videos
+    n = 0
+    for child in children:
+        ### (a)Prepare videos (crop if necessary):
+        vid_path = Path(f"{folder}/{child}/")
+        ## The first camera
+        vid1_list = glob.glob(f"{vid_path}/*{cam1}*.mp4")
+        if len(vid1_list) < 1: ##check for problem
+            print(f"Can't find {child}'s {cam1}. Please check the path and folder names.")
+        else:
+            vid1 = VideoFileClip(vid1_list[0])
+            if crop_who == cam1:
+                vid1 = vid1.crop(x1=x1[n], x2=x2[n], y1=y1[n], y2=y2[n])
+            ## The second camera
+            vid2_list = glob.glob(f"{vid_path}/*{cam2}*.mp4")
+            if len(vid2_list) < 1: ##check for problem
+                print(f"Can't find {child}'s {cam2}. Please check the path and folder names.")
+            else:
+                vid2 = VideoFileClip(vid2_list[0])
+                if crop_who == cam2:
+                    vid2 = vid2.crop(x1=x1[n], x2=x2[n], y1=y1[n], y2=y2[n])
+                ### (b)Sync the videos:
+                t_vid1 = vid1_list[0][-10:-5]
+                t_vid1 = t_vid1.replace("M", ":")
+                t_vid1 = datetime.strptime(t_vid1, "%M:%S")
+                t_vid2 = vid2_list[0][-10:-5]
+                t_vid2 = t_vid2.replace("M", ":")
+                t_vid2 = datetime.strptime(t_vid2, "%M:%S")
+                diff = t_vid1 - t_vid2
+                diff = diff.total_seconds()
+                ## cam1
+                if vid1.duration > end[n]:
+                    vid1 = vid1.subclip(start[n], end[n])
+                else:
+                    vid1 = vid1.subclip(start[n], vid1.duration)
+                ## cam2
+                if attempts == 1: ##first round
+                    x = 0 ##no manual correction
+                    if vid2.duration > end[n]+diff:
+                        vid2 = vid2.subclip(start[n], end[n]+diff)
+                    else:
+                        vid2 = vid2.subclip(start[n], vid2.duration)
+                    ##save audio files to aid correction of out-of-sync videos:
+                    audio_vid1 = vid1.audio
+                    audio_vid1.write_audiofile(f"{folder}/{child}/{child}_{cam1}_audio.mp3")
+                    audio_vid2 = vid2.audio
+                    audio_vid2.write_audiofile(f"{folder}/{child}/{child}_{cam2}_audio.mp3")
+                elif attempts > 1: ##corrective round
+                    x = corr[n]
+                    if vid2.duration > end[n]+diff+corr[n]:
+                        vid2 = vid2.subclip(start[n]+corr[n], end[n]+diff+corr[n])
+                    else:
+                        vid2 = vid2.subclip(start[n]+corr[n], vid2.duration)
+                ### (c) Deal with the volume/audio extraction:
+                ##amplify if necessary and add a blue border
+                if amplify_who == cam1:
+                    vid1 = vid1.volumex(amplify).margin(10, color=(0, 0, 225))
+                    vid2 = vid2.margin(10)
+                elif amplify_who == cam2:
+                    vid2 = vid2.volumex(amplify).margin(10, color=(0, 0, 225))
+                    vid1 = vid1.margin(10)
+                ##mute if necessary
+                if mute_who == cam1:
+                    vid1 = vid1.volumex(0)
+                elif mute_who == cam2:
+                    vid2 = vid2.volumex(0)
+                ### (d) Position videos and save them:
+                if main[n] == cam1:
+                    major, minor = vid1, vid2
+                else:
+                    major, minor = vid2, vid1
+                ##resize videos
+                major_vid = major.resize(0.7)
+                minor_vid =  minor.resize(0.4)
+                final_vid = clips_array([[major_vid, minor_vid], ])
+                ##save the output
+                final_vid.write_videofile(f"{folder}/{child}/{child}_{newname}_{attempts}_corr={x}.mp4")
+                n += 1 ##now, do the next one
--- python/examples.py
+++ python/examples.py
@@ -1,58 +1,89 @@
 #### Affiliation: SW-Lab, Dept of CFS, NTNU
-#### Date: 31.03.2025
+#### Date: 02.04.2025
 #### Author: MY Sia (with lots of help from the web, see README.md for more)
-#### Example scripts to use the "editvid" function. Uses MoviePy v1.0.3
+#### Example scripts to use the "editvid" function. Requires MoviePy v1.0.3
 
 ##------------------------##
 ##EXAMPLE 1: MERGE VIDEOS
 ##Concatenate short videos into a long one; batch process several cameras of several children
-##Required directory: project folder -> child folder -> camera folder -> short videos (e.g., <desktop>/Peekaboo/P01/BABY/<videos>)
+##Required directory: project folder -> "child" subfolder -> "camera" subfolder -> short videos
 from editvid import merge
 merge(folder="C:/Users/user/Desktop/mc_vid", ##set path to the project folder
-      children=["a62_c62", "a63_c63", "a64_c64"], ##which child folder are we processing?
-      camera=["BABY", "front", "SBR1", "SCREEN", "影片二"]) ##which camera folder are we processing?
+      children=["a62_c62", "a63_c63", "a64_c64"], ##which 1st-level folder are we processing?
+      camera=["BABY", "front", "SBR1", "SCREEN", "影片二"]) ##which 2nd-level folder are we processing?
 
 ##------------------------##
 ##EXAMPLE 2: SYNC AND OVERLAY VIDEOS
 ##Sync and overlay a downsized SCREEN video on a BABY video for gaze coding
-##Required directory: project folder -> child folder -> videos (e.g., <desktop>/Peekaboo/P01/<videos>)
+##Required directory: project folder -> "child" subfolder -> videos
 from editvid import overlay
 
 ##EXAMPLE 2a: Using info from an excel file
 overlay(folder = "C:/Users/user/Desktop/mc_vid", ##set path to the project folder
         attempts = 1, ##1: correction info will be disregarded, 2 or higher: correction info will be needed
-        bgcam = "baby", ##name of the background video that python should search for
-        topcam = "screen", ##name of the to-be-overlaid video that python should search for
+        bgcam = "baby", ##name of the base video that python should search for
+        topcam = "screen", ##name of the top video that python should search for
         newname = "OMI", ##name of the output video
-        propsize = 0.25, ##resize the to-be-overlaid video
-        dur = None,
+        propsize = 0.25, ##resize the top video
+        dur = None, ##duration of the recorded task (if it is the same for everyone)
         excel = "C:/Users/user/Desktop/mc_vid/example_overlay.xlsx",
         children=None, start=None, end=None, corr=None)
 
 ##EXAMPLE 2b: Using manually entered info
-overlay(folder = "C:/Users/user/Desktop/mc_vid", ##set path to the project folder
-        attempts = 1, ##1: correction info will be disregarded, 2 or higher: correction info will be needed
-        bgcam = "baby", ##name of the background video that python should search for
-        topcam = "screen", ##name of the top video that python should search for
-        newname = "OMI", ##name of the output video
-        propsize = 0.25, ##resize the to-be-overlaid video
+overlay(folder = "C:/Users/user/Desktop/mc_vid",
+        attempts = 1,
+        bgcam = "baby",
+        topcam = "screen",
+        newname = "OMI",
+        propsize = 0.25,
         dur = None,
         excel = None, ##if not attaching an excel, enter info below
-        children = ["076", "078"], ##which child folder are we processing?
-        start = [20, 19], ##the seconds at which the experiment STARTED
-        end = [238, 609], ##the seconds at which the experiment ENDED
-        corr = [-1, 0.9]) ##manually correct out-of-sync videos
+        children = ["076", "078"], ##which 1st-level folder are we processing?
+        start = [20, 19], ##the seconds at which the task STARTED, based on topcam
+        end = [238, 609], ##the seconds at which the task ENDED
+        corr = [-1, 0.9]) ####if topcam lags behind, give a negative number
 
 ##------------------------##
-##EXAMPLE 3a:
-##
-##Required directory:
+##EXAMPLE 3: CROP VIDEOS
+##Crop videos
+##Required directory: project folder -> "child" subfolder -> videos
 from editvid import crop
 crop(folder = "C:/Users/user/Desktop/mc_vid", ##set path to the project folder
-     cam = "front", ##name of the video that python should search for
+     cam = "front", ##name of videos that python should crop
      newname = "solo", ##name of the output video
-     dur = 183, ##duration of the recording
-     amplify = 5, ##do we want to mute/amplify the volume of the video?
-     excel = "C:/Users/user/Desktop/mc_vid/mochibaby.xlsx",
-     children=None, start=None, end=None, x1=None, x2=None, y1=None, y2=None)
-##See Example 2b for manually entered info
+     dur = 183, ##duration of the recorded task (if it is the same for everyone)
+     amplify = 5, ##do we want to amplify the volume of the video? 0=mute
+     excel = None,
+     children = [c63, c64],
+     start = [51, 459],
+     end = [],
+     x1 = [644, 644], ##start of the width that we want to crop
+     x2 = [2254, 2254], ##end of the width that we want to crop
+     y1 = [175, 170], ##start of the height that we want to crop
+     y2 = [1092, 1092]) ##end of the height that we want to crop
+
+##------------------------##
+##EXAMPLE 4: SYNC AND JUXTAPOSE VIDEOS
+##Sync the time of two videos and place them side-by-side with one video larger than the other
+##Required directory: project folder -> "child" subfolder -> videos
+from editvid import join2side
+join2side(folder = "C:/Users/user/Desktop/mc_vid",
+        attempts = 3,
+        cam1 = "front", ##name of the first video
+        cam2 = "side", ##name of the second video
+        newname = "sbr",
+        dur = 305, ##duration of the recorded task (if it is the same for everyone)
+        amplify_who = "side", ##which video should we amplify? "no" if neither
+        amplify = 10, ##how much to amplify? 0 will mute the video
+        mute_who = "front", ##which video should we mute? "no" if neither
+        crop_who = "front", ##which video should we crop? "no" if neither
+        excel = None,
+        children = ["c62", "c63"],
+        main = ["front", "front"], ##main is the video with the best angle
+        start = [73, 334], ##the seconds at which the task STARTED, based on cam1
+        end = None,
+        corr = [-1.9, 0.8], ##if cam1 lags behind, give a negative number
+        x1 = [644, 644], ##indicates the area of the video that we want to crop
+        x2 = [2254, 2254], ##see example 3 for more
+        y1 = [160, 155],
+        y2 = [1092, 1092])
