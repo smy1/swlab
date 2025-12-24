a7f5285285bbd1e12aee912a1169fbb8164b835b
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2025-12-24 18:21:08 +0800

    Add files via upload

--- obsolete/merge-vid2.py
+++ obsolete/merge-vid2.py
@@ -38,22 +38,26 @@ for child in childlist:
                 vid=file_list[i]
                 clip=VideoFileClip(file_list[i])
                 video_list.append(clip)
-                wanted_t=vid[-10:-5] ##extract the start time of the video
-                ##for the first video
-                if i==0:
-                    end1=clip.duration
-                    start_sec=(float(wanted_t[:2])*60)+float(wanted_t[-2:]) ##time lapsed to the wanted first frame
-                    to_add=timedelta(hours=0, minutes=float(wanted_t[:2]), seconds=float(wanted_t[-2:]))
-                    t_frame=f"{file_list[0][x+26:x+28]}:{file_list[0][x+28:x+30]}" ##the time of the video's first frame
-                    t_frame=datetime.strptime(t_frame, "%M:%S")
-                    t_start=t_frame+to_add
-                    frame=t_start.strftime("%Y-%m-%d %H:%M:%S")
-                ##for the other videos
-                else:
-                    if "前" in vid[-5:-4]: ##time lapsed till the end of the wanted recording
-                        end_sec+=(float(wanted_t[:2])*60)+float(wanted_t[-2:])
+                try: ##if the name of the recording has time duration
+                    wanted_t=vid[-10:-5] ##extract the start time of the video
+                    ##for the first video
+                    if i==0:
+                        end1=clip.duration
+                        start_sec=(float(wanted_t[:2])*60)+float(wanted_t[-2:]) ##time lapsed to the wanted first frame
+                        to_add=timedelta(hours=0, minutes=float(wanted_t[:2]), seconds=float(wanted_t[-2:]))
+                        t_frame=f"{file_list[0][x+26:x+28]}:{file_list[0][x+28:x+30]}" ##the time of the video's first frame
+                        t_frame=datetime.strptime(t_frame, "%M:%S")
+                        t_start=t_frame+to_add
+                        frame=t_start.strftime("%Y-%m-%d %H:%M:%S")
+                    ##for the other videos
                     else:
-                        end_sec+=clip.duration
+                        if "前" in vid[-5:-4]: ##time lapsed till the end of the wanted recording
+                            end_sec+=(float(wanted_t[:2])*60)+float(wanted_t[-2:])
+                        else:
+                            end_sec+=clip.duration
+                except:
+                    start_sec=0
+                    frame="00:00:00"
             joined=concatenate_videoclips(video_list)
             joined=joined.resize(height=720).set_fps(fps=30)
             final=joined.subclip(start_sec, end_sec+end1)
